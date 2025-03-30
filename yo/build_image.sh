745d9752f3763e7361e786e36500608c70cee0f2
AlexanderDemachev <berserktv@yandex.ru>
2025-03-30 14:47:46 +0300

    docker,ubuntu-22-04: for core-image-minimal

--- yo/build_image.sh
+++ yo/build_image.sh
@@ -4,8 +4,7 @@ this_d=$(dirname "$this_f")
 source $this_d/func.sh
 
 cmd_runs="$1"
-DOCKER_DIR="docker/ubuntu_18_04"
-CONTAINER_NAME="ubuntu_18_04"
-cmd_init="cd /mnt/docker/ubuntu_18_04; MACHINE=$YO_M; source ./setup-environment build"
-
+DOCKER_DIR="docker/ubuntu_24_04"
+CONTAINER_NAME="ubuntu_24_04"
+cmd_init="cd /mnt/data; MACHINE=$YO_M source ./setup-environment build"
 start_cmd_docker "${cmd_init}; ${cmd_runs}"
