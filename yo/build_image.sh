5ace6875d7b78d69d7341254ff3b58cbd7ec9699
AlexanderDemachev <berserktv@yandex.ru>
2025-04-01 21:47:45 +0300

    docker: for YoctoDemoMinimal (Ubuntu-22.04)

--- yo/build_image.sh
+++ yo/build_image.sh
@@ -4,7 +4,7 @@ this_d=$(dirname "$this_f")
 source $this_d/func.sh
 
 cmd_runs="$1"
-DOCKER_DIR="docker/ubuntu_24_04"
-CONTAINER_NAME="ubuntu_24_04"
+DOCKER_DIR="docker/ubuntu_22_04"
+CONTAINER_NAME="ubuntu_22_04"
 cmd_init="cd /mnt/data; MACHINE=$YO_M source ./setup-environment build"
 start_cmd_docker "${cmd_init}; ${cmd_runs}"
