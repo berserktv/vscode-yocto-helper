4f61972bd3c51fea5c8ede9f61dd577a94e890d9
AlexanderDemachev <berserktv@yandex.ru>
2025-04-12 23:02:57 +0300

    install: changed the installation script for Ubuntu 22.04 snap for 24.04 snapd and the user needs to restart the session

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 DIR_INSTALL="$HOME/yocto/vscode-yocto-helper"
 
-sudo apt install -y snap
+sudo apt install -y snap || sudo apt install -y snapd
 sudo snap install --classic code
 code --install-extension seunlanlege.action-buttons
 
@@ -12,14 +12,14 @@ sudo apt install -y docker.io
 
 sudo usermod -aG docker $USER
 sudo usermod -aG dialout $USER
-exec su - $USER
 
 mkdir -p "${DIR_INSTALL}"
 cd "${DIR_INSTALL}"
-
 git clone https://github.com/berserktv/vscode-yocto-helper.git .vscode
-code .
 
-
-
-
+echo ""
+echo "Important: Changes to 'docker' group membership require a new login session."
+echo "Please completely log out and log back into your system to apply changes."
+echo ""
+echo "Project will be installed to: ${DIR_INSTALL}"
+echo ""
